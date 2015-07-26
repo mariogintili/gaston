03924fcdc1869ed4a5b8e35865b3c09df4c30f17
mariogintili <mariojgintili@gmail.com>
2015-07-26 12:07:27 +0100

    first commmit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gaston
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gaston/core_test.clj
@@ -0,0 +1,7 @@
+(ns gaston.core-test
+  (:require [clojure.test :refer :all]
+            [gaston.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
