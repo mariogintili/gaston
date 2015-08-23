c365a57ecdd7dadf1e5d0f8f1eb73ac4b698cacf
mariogintili <mariojgintili@gmail.com>
2015-08-23 12:07:05 +0100

    serve the value of the bootstrapped string

--- src/gaston/core.clj
+++ src/gaston/core.clj
@@ -1,11 +1,20 @@
 (ns gaston.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty])
+  (:use ring.middleware.params))
+
+(defn bootstrap-index [{query-params :query-params}]
+  "Look for an index key, replace with 'current' if not found"
+  (pr query-params)
+  (def redis_key (get query-params "index_key" "current"))
+  (pr redis_key)
+  (str "<codecards>:" (str redis_key)))
 
 (defn request-handler [request]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello Clojure, Hello Ring!"})
+   :body (bootstrap-index request)})
+
 
 (defn -main []
   "I don't do a whole lot ... yet."
-  (jetty/run-jetty request-handler {:port 3000}))
+  (jetty/run-jetty (wrap-params request-handler) {:port 3000}))
