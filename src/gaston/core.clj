0ff97d7dd61b1ceca54a4d9fb0cea52eafb0bbbe
mariogintili <mariojgintili@gmail.com>
2015-08-24 20:24:07 +0100

    setting up redis, we can now read from it

--- src/gaston/core.clj
+++ src/gaston/core.clj
@@ -1,19 +1,23 @@
 (ns gaston.core
   (:require [ring.adapter.jetty :as jetty])
   (:require [environ.core :refer [env]])
-  (:use ring.middleware.params))
+  (:use ring.middleware.params)
+  (:require [taoensso.carmine :as car :refer (wcar)]))
 
 (defn bootstrap-index [{query-params :query-params}]
   "Look for an index key, replace with 'current' if not found"
-  (pr query-params)
   (def redis_key (get query-params "index_key" "current"))
-  (pr redis_key)
   (str "<codecards>:" (str redis_key)))
 
+(def redis-connection {:pool {} :spec {:host (or (env :redis-url) "localhost") :port (or (env :redis-port) 6379) }} )
+(defmacro wcar* [& body] `(car/wcar redis-connection ~@body))
+
 (defn request-handler [request]
+  (def index_key (bootstrap-index request))
+  (def server-connection {})
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (bootstrap-index request)})
+   :body (wcar* (car/get index_key))})
 
 
 (defn -main [& [port]]
