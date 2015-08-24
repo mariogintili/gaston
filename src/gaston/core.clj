104469071457fb5a732a8c721598badf4b81b91f
mariogintili <mariojgintili@gmail.com>
2015-08-24 20:50:22 +0100

    look for a URI instead

--- src/gaston/core.clj
+++ src/gaston/core.clj
@@ -9,7 +9,7 @@
   (def redis_key (get query-params "index_key" "current"))
   (str "<codecards>:" (str redis_key)))
 
-(def redis-connection {:pool {} :spec {:host (or (env :redis-url) "localhost") :port (or (env :redis-port) 6379) }} )
+(def redis-connection {:pool {} :spec {:uri (or (env :redis-url) "redis://localhost:6379") }} )
 (defmacro wcar* [& body] `(car/wcar redis-connection ~@body))
 
 (defn request-handler [request]
