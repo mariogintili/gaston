d8d71e98147e285584faaeffe3247e4d26d7752c
mariogintili <mariojgintili@gmail.com>
2015-08-31 12:56:24 +0100

    fix scoping

--- src/gaston/core.clj
+++ src/gaston/core.clj
@@ -4,12 +4,13 @@
   (:use ring.middleware.params)
   (:require [taoensso.carmine :as car :refer (wcar)]))
 
+(def redis-connection {:pool {} :spec {:uri (env :redis-url)}})
+(defmacro wcar* [& body] `(car/wcar redis-connection ~@body))
+
 (defn bootstrap-index [{query-params :query-params}]
   "Look for an index key, replace with 'current' if not found"
   (get query-params "index_key" (wcar* (car/get "codecards:current"))))
 
-(def redis-connection {:pool {} :spec {:uri (env :redis-url)}})
-(defmacro wcar* [& body] `(car/wcar redis-connection ~@body))
 
 (defn request-handler [request]
   {:status 200
