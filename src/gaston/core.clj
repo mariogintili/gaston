f984e5fb04206a3934584718e9b83e6787f045c9
mariogintili <mariojgintili@gmail.com>
2015-08-31 12:46:19 +0100

    refactor

--- src/gaston/core.clj
+++ src/gaston/core.clj
@@ -6,19 +6,15 @@
 
 (defn bootstrap-index [{query-params :query-params}]
   "Look for an index key, replace with 'current' if not found"
-  (def redis_key (get query-params "index_key" "current"))
-  (str "codecards:" (str redis_key)))
+  (get query-params "index_key" "codecards:current"))
 
 (def redis-connection {:pool {} :spec {:uri (env :redis-url)}})
 (defmacro wcar* [& body] `(car/wcar redis-connection ~@body))
 
 (defn request-handler [request]
-  (def index_key (bootstrap-index request))
-
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (wcar* (car/get index_key))})
-
+   :body (wcar* (car/get (bootstrap-index request)))})
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
