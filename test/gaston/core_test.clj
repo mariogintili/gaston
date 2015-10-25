f656007dfd226076ec75587f8ae475ac0f68372d
mariogintili <mariojgintili@gmail.com>
2015-10-25 10:03:50 +0000

    add test to cover for the presence of an index key

--- test/gaston/core_test.clj
+++ test/gaston/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [gaston.core :refer :all]
             [ring.mock.request :as mock]
-            [taoensso.carmine :as car :refer (wcar)]))
+            [taoensso.carmine :as car :refer (wcar)])
+  (:use ring.middleware.params))
 
 (defn setup-env []
   (wcar* (car/set "ember-app-name:current" "ember-app-name:default-revision"))
@@ -12,6 +13,7 @@
 (defn teardown-env []
   (wcar* (car/flushall)))
 
+
 (defn gaston-test-setup [f]
   (setup-env)
   (f)
@@ -20,7 +22,21 @@
 (use-fixtures :once gaston-test-setup)
 
 (deftest request-without-an-index-key
-  (is (= {:status 200
-          :headers {"Content-Type" "text/html"}
-          :body "default-revision-value"}
-          (request-handler (mock/request :get "/")))))
+  (is
+    (= {:status 200
+        :headers {"Content-Type" "text/html"}
+        :body "default-revision-value"}
+        ((wrap-params request-handler) (mock/request :get "/"))
+    )
+  )
+)
+
+(deftest request-with-an-index-key
+  (is
+    (=  {:status 200
+         :headers {"Content-Type" "text/html"}
+         :body "old-revision"}
+         ((wrap-params request-handler) (mock/request :get "?index_key=ember-app-name:old-revision"))
+    )
+  )
+)
